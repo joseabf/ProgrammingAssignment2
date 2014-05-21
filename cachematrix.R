0acb9f779cffe4c889253ee1f7abad7f973cbf68
joseabf <joseabf@gmail.com>
2014-05-21 19:30:02 +0200

    week3 project

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix<- function(x=numeric()){
+        ##x is the matrix that we want calculate
+        m<-NULL        ##initial value m is NULL, we have not calculated inverse yet
+        set<-function(y){
+                x<<-y
+                m<<-NULL    ##initial value matrix x set to y
+        }
+        get<-function() x     ## this function "get" value of matrix
+        setinverse<-function(z) m<<-z     #set value inverse in m (now m is not NULL)
+        getinverse<-function() m        ##get matrix inverse value
+        list (set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve<-function (x,...){
+   ## Return a matrix that is the inverse of 'x'
+        m<-x$getinverse()   ##m receive value inverse
+        if(!is.null(m)){
+                ##if m is not null return m and message, end function
+                message("getting checked data")
+                return (m)
+        }
+        data<-x$get()   ##if m is NULL we get the matrix "x"
+        m<-solve(data)  ##we calculate value inverse
+        x$setinverse(m) ##we set m to value no NULL
+        m
 }
